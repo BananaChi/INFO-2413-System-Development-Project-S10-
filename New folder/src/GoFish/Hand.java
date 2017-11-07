842bbb718f357f82ce2d72e044a2168cde346c80
ravnitdeo <ravnitdeo97@yahoo.com>
2017-11-06 16:40:56 -0800

    Edited Hand

--- New folder/src/GoFish/Hand.java
+++ New folder/src/GoFish/Hand.java
@@ -1,20 +1,51 @@
 /**
- * 
+ * This class represents a hand: the cards held by one player.
  */
-package GoFish;
-
-/**
- * @author Anna
- *
- */
-public class Hand {
-
+public class Hand extends CardArray{
+	
 	/**
-	 * @param args
+	 * Instantiates a new hand.
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Hand() {
+		super();
 	}
+	
+    /**
+     * Pop card: removes and returns the card in Hand at 
+     * the given index.
+	 * <p>
+	 * Note: this method overloads popCard() in CardArray class.
+	 * </p>
+     *
+     * @param i, the (zero-based) index of a card in Hand.
+     * @return the card
+     */
+    public Card popCard(int i) {
+    	Card card = cards.remove(i);
+    	return card;
+    }
+	
+    /**
+     * Gets the card: returns (but does not remove) the card 
+     * in Hand at the given index.
+     *
+     * @param i, the (zero-based) index of a card in Hand.
+     * @return the card
+     */
+    public Card getCard(int i) {    	
+    	return cards.get(i);
+    }
 
+    /* Returns a string representation of the Hand. */
+    @Override
+    public String toString() {
+    	if(cards.isEmpty())
+    		return "empty!";
+    	
+    	String output = "";
+    	for(int i=0; i<cards.size(); i++){
+    		output += "[" + Integer.toString(i+1) + "]: " + cards.get(i) + "\n";
+    	}
+    	return output;
+    }
 }
