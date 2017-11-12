72738f71bd766936be833ddfddacfc3265bd455c
ravnitdeo <ravnitdeo97@yahoo.com>
2017-11-11 17:23:22 -0800

    User class

--- New folder/src/GoFish/User.java
+++ New folder/src/GoFish/User.java
@@ -7,12 +7,18 @@ public class User extends Player {
 	/** The input. */
 	private Scanner input;
 	
-	public void createAccount (String username, String password) {
-		
+	/**
+	 * Instantiates a human player with an empty hand.
+	 *
+	 * @param name, the name of the player
+	 */
+	public User(String name){
+		super(name);
+		input = new Scanner(System.in);
 	}
 	
 	public Card play(Game GoFish) {
-		
+		return null;
 	}
 
 	public void saveScore(int scores) {
